ce6a92c41d270ae09d964c91a7deeceed9f8abf0
NCJ <iamncj@outlook.com>
2020-05-08 10:18:27 +0800

    into c++

---
+++ Interpreter/Interpreter.h
@@ -0,0 +1,14 @@
+//
+// Created by NCJ on 5/8/2020.
+//
+
+#ifndef MINISQL_INTERPRETER_H
+#define MINISQL_INTERPRETER_H
+
+
+class Interpreter {
+
+};
+
+
+#endif //MINISQL_INTERPRETER_H
---
+++ main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by NCJ on 5/8/2020.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "Welcome to miniSQL!" << std::endl;
+
+}
