f7cf6ebf77b1709c4252650f598167b2d6b66a2e
NCJ <iamncj@outlook.com>
2020-05-08 10:13:03 +0800

    rm java

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import Interpreter.Interpreter;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Welcome to miniSQL!");
-        Interpreter interpreter = new Interpreter();
-        interpreter.main_loop();
-    }
-}
