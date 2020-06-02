145491726175acfb4fff344d7178a64a9fed53cf
MartinNose <liucmartin@qq.com>
2020-06-02 22:05:00 +0800

    test

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 
-#include "Interpreter.h"
+#include "BufferManager.h"
+#include "RecordManager.h"
+#include "IndexManager.h"
+#include "DataStructure.h"
 
 int main() {
     std::cout << "Welcome to miniSQL!" << std::endl;
-    Interpreter interpreter;
-    interpreter.main_loop();
+    BufferManager bm;
+    IndexManager im;
+    RecordManager rm(&bm, &im);
+
+    rm.createTable("student");
+    Table t;
+
 }
